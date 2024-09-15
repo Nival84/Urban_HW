59b9c6e20bf683519263be504fb90aa5ed1a9dd4
Nival84 <rednival1984@gmail.com>
2024-09-15 20:01:48 +0300

    15.09_main-test

--- lesson0/main.py
+++
@@ -1,9 +0,0 @@
-name = "denis"
-print(name [::-1])
-animals           = ['python','gopher']
-more_animals      = animals
-print(animals == more_animals)
-print('-'.join(['a','b','c']))
-# test git
-print(animals)
-
