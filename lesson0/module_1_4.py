f640ad5954a98e0c4ed8fa68fef3fe3c159cd747
Nival84 <rednival1984@gmail.com>
2024-09-15 18:19:47 +0300

    15.09

---
+++ lesson0/module_1_4.py
@@ -0,0 +1,12 @@
+my_string = input("Укажите Ваше имя: ")
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ""))
+print(my_string [0])
+print(my_string [-1])
+
+
+
+
+
+
