b40162dd4b1801f1f43823b5967254a3d1033842
Nival84 <rednival1984@gmail.com>
2024-09-15 19:55:20 +0300

    15.09

---
+++ lesson0/module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = (1,2, True, 'name', [3,4], ["test1"]) #Кортеж не изменин. Менять можно только  обьекты внутри кортежы.
+print(immutable_var)
+immutable_var[4][0] = 6
+immutable_var[5][0] = 'T1'
+print(immutable_var)
+mutable_list = [1,2,'sort']
+print(mutable_list)
+mutable_list[2] = '3 start'
+print(mutable_list)
